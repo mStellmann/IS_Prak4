920ee46d0c3efa9aa0f5519330791b276a8ea408
mStellmann <matthias.stellmann@haw-hamburg.de>
2014-06-18 16:14:56 +0200

    Update sudokuLoesung3.pl

--- sudokuLoesung3.pl
+++ sudokuLoesung3.pl
@@ -1,3 +1,6 @@
+:- use_module(library(clpfd)).
+:- consult(spielfelder).
+
 % Loesung - Hart Codiert
 sudoku(Spielfeld_Zeilen) :-
   Spielfeld_Zeilen = [Zeile1, Zeile2, Zeile3, Zeile4, Zeile5, Zeile6, Zeile7, Zeile8, Zeile9],
